7fdd67c86ffba101e31fdeb1a5b03444aa4f68e0
DoubleQuotationMarks <58721772+DoubleQuotationMarks@users.noreply.github.com>
2021-11-18 14:34:20 +0800

    Update Register.js

--- frontend/src/Component/Register.js
+++ frontend/src/Component/Register.js
@@ -4,9 +4,9 @@ import { useState, useEffect } from 'react';
 const Register = ({ userID, password, setRegisterStr }) => {
 
   useEffect(() => {
-    axios.post('../user', {         //某種function，傳入使用者帳密進入資料庫中，並確認是否有重複使用者ID(假設回傳一個字串)
-      "UserID": userID,
-      "Password": password
+    axios.post('http://127.0.0.1:8000/api/signup', {         //某種function，傳入使用者帳密進入資料庫中，並確認是否有重複使用者ID(假設回傳一個字串)
+      "username": userID,
+      "password": password
     })
     .then((res) => { 
       setRegisterStr(res.data)
