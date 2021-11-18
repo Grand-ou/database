4f4511fffcdce9d2891840687a631fb353fccb42
DoubleQuotationMarks <58721772+DoubleQuotationMarks@users.noreply.github.com>
2021-11-18 13:48:18 +0800

    Update Login.js

--- frontend/src/Component/Login.js
+++ frontend/src/Component/Login.js
@@ -4,9 +4,9 @@ import { useState, useEffect } from 'react';
 const Login = ({ userID, password, setLoginStr }) => {
 
   useEffect(() => {
-    axios.post('../user', {         //某種function，傳入使用者帳密進入資料庫中，並確認是否有重複使用者ID(假設回傳一個字串)
-      "UserID": userID,
-      "Password": password
+    axios.post('http://127.0.0.1:8000/api/login', {         //某種function，傳入使用者帳密進入資料庫中，並確認是否有重複使用者ID(假設回傳一個字串)
+      "username": userID,
+      "password": password
     })
     .then((res) => { 
       setLoginStr(res.data)
