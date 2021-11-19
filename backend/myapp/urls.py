e1d07af5307a68a6b8de79525c25e48cd6316d64
judycpc <abc0952753111@gmail.com>
2021-11-19 16:18:11 +0800

    urls updated

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -6,5 +6,12 @@ urlpatterns = [
     url('login',  views.login_request),
     url('stockinformation', views.stockinformation),
     url('seestrategy', views.seestrategy),
-    #url('backtest', views.backtest),
+    url('create/rsi', views.rsi_create),
+    url('create/macd', views.macd_create),
+    url('create/kd', views.Kd_create),
+    url('create/ema', views.Ema_create),
+    url('backtest/rsi', views.back_test_rsi),
+    url('backtest/macd', views.back_test_macd),
+    url('backtest/kd', views.back_test_kd),
+    url('backtest/ema', views.back_test_ema),
 ]
