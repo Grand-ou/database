413c79d688db17ed9b376ee47c3160aa88463bb8
DoubleQuotationMarks <58721772+DoubleQuotationMarks@users.noreply.github.com>
2021-11-20 23:35:56 +0800

    Update Profit_Loss.js

--- frontend/src/Container/Profit_Loss.js
+++ frontend/src/Container/Profit_Loss.js
@@ -2,11 +2,11 @@ import '../App.css'
 import { Space, Button } from 'antd'
 import Backtest from '../Component/Backtest'
 
-const Profit_Loss = ({ company, indicator, num1, num2, stopProfit, stopLoss, setClickCheck, setClickCreate }) => {
+const Profit_Loss = ({ company, indicator, num1, num2, budget, stopProfit, stopLoss, setClickCheck, setClickCreate }) => {
 
     return(
         <div className='profit-loss'>
-            <Backtest company={company} indicator={indicator} num1={num1} num2={num2} stopProfit={stopProfit} stopLoss={stopLoss} />
+            <Backtest company={company} indicator={indicator} num1={num1} num2={num2} stopProfit={stopProfit} stopLoss={stopLoss} budget={budget} />
             <Button onClick={()=>{setClickCheck(false); setClickCreate(true);}}>回上頁</Button>
         </div>
     )
