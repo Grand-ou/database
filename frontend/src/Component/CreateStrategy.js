b1e14f52420477db3f7e58802424e61e4a478e37
DoubleQuotationMarks <58721772+DoubleQuotationMarks@users.noreply.github.com>
2021-11-20 10:52:35 +0800

    Update CreateStrategy.js

--- frontend/src/Component/CreateStrategy.js
+++ frontend/src/Component/CreateStrategy.js
@@ -40,6 +40,20 @@ const CreateStrategy = ({ company, indicator, num1, num2, stopProfit, stopLoss, 
       })
       .catch((error) => { console.log(error) })
     }
+    else if (indicator == 'KD'){
+      axios.post('http://127.0.0.1:8000/api/create/kd', {         //某種function，傳入各種數值進入策略的資料庫中
+      "Creator_id": userID,
+      "Company_id": company,
+      "Length": num1,
+      "Threshold": num2,
+      "Profit": stopProfit, 
+      "Loss": stopLoss
+      })
+      .then((res) => { 
+        setStrategy(res.data)
+      })
+      .catch((error) => { console.log(error) })
+    }
       
   },[])
 
